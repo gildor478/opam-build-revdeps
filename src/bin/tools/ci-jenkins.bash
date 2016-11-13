0bc2d648bd47c361dfaf0a617e3bb514d8bb32d2
Sylvain Le Gall <sylvain@le-gall.net>
2016-11-13 20:12:55 +0100

    Add missing 'opam install' call.

--- src/bin/tools/ci-jenkins.bash
+++ src/bin/tools/ci-jenkins.bash
@@ -1,5 +1,6 @@
 . "$(dirname "$0")/ci-opam.bash" || exit 1
 . "$(dirname "$0")/ci-packages.bash" || exit 1
 export OCAMLRUNPARAM=b
+opam install $OPAM_PKGS
 ocaml setup.ml -configure
 ocaml setup.ml -build
